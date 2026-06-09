101bf845150d505b30a9b7bc6395f5ea2af4ff25
式田勇樹 <y.shikita@RV232-3.local>
2026-06-09 14:11:57 +0900

    フォーカス解除のpushを追加

--- lib/extensions/navigator_extensions.dart
+++ lib/extensions/navigator_extensions.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 extension NavigatorExtensions on NavigatorState{
+  /// フォーカスを解除してから次の画面を `push` する。
+  ///
+  /// [pop] 時に値を受け取りたい場合は、`await` すること。
+  Future<T?> pushWithUnfocus<T extends Object?>(Route<T> route) async {
+    // NavigatorStateのcontextを使ってフォーカスを解除
+    FocusScope.of(context).unfocus();
+
+    // 次の画面へ遷移
+    return await push(route);
+  }
   /// フォーカスを解除してから画面を閉じる
   void popWithUnfocus<T>([T? result]) {
     // NavigatorStateのcontextを使ってフォーカスを解除
